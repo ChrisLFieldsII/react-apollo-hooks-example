423036b61829bf3e6f22d14948bf86521f3c2bb0
Wicked Worlok <chrisjr06@gmail.com>
2019-09-14 13:11:11 -0500

    feat(mobile): + colorwheel pkg. setup CreateUser mutation & subsxns
    The UserForm works and allows creating user.
    It however needs functionality for Edit User
    
    Subscriptions use same technique as web, just refetch

--- mobile/src/screens/User/User.js
+++ mobile/src/screens/User/User.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, FlatList, SafeAreaView, TextInput, KeyboardAvoidingView, Keyboard} from 'react-native';
 import { useQuery, useMutation, useSubscription, useApolloClient } from '@apollo/react-hooks';
+import { ColorWheel } from 'react-native-color-wheel';
+import colorsys from 'colorsys';
 
 import { listUsers } from '../../gql/queries';
 import { createUser, updateUser, deleteUser } from '../../gql/mutations';
@@ -8,31 +10,122 @@ import { onCreateUser, onUpdateUser, onDeleteUser } from '../../gql/subscription
 import { noop, getHeight, getWidth, COLORS } from '../../utils';
 
 const NUM_COLUMNS = 1;
+const INITIAL_COLOR = '#ffffff';
 
-const styles = {
+const styles = StyleSheet.create({
   center: {
     justifyContent: 'center',
     alignItems: 'center',
   },
-}
+  container: {
+    flex: 1,
+  }
+});
+
+// #region USER_FORM
+const userFormStyles = StyleSheet.create({
+  textInput: {
+    width: getWidth(),
+    height: 40,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+  },
+  formContainer: {
+    marginBottom: 10,
+  },
+  submitBtn: {
+    width: getWidth() * 0.8,
+    alignSelf: 'center',
+    backgroundColor: COLORS.BLUE,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  colorwheelContainer: {
+    width: getWidth(),
+    height: getHeight() * 0.4,
+    marginBottom: 30,
+  },
+  colorwheelThumb: {
+    height: 15,
+    width: 15,
+    borderRadius: 15,
+  },
+})
 
 function UserForm({ stateTuple, setIsRenderForm }) {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [color, setColor] = useState(INITIAL_COLOR);
   // const [formState, setFormState] = stateTuple;
 
   const [createUserFn] = useMutation(createUser);
   const [updateUserFn] = useMutation(updateUser);  
 
-  const closeForm = () => setIsRenderForm(false);
+  const onColorChangeComplete = color => setColor(colorsys.hsv2Hex(color.h, color.s, color.v));
+
+  const doCreateUser = async () => {
+    if (!name) {
+      alert('Must input name!')
+      return;
+    }  
+
+    try {
+      const {data} = await createUserFn({
+        variables: {
+          input: {
+            name: name.trim(),
+            age: parseInt(age),
+            favColor: color,
+          }
+        }
+      });
+
+      console.log(data);
+      alert(`Successfully created user ${name}!`);
+    } catch (err) {
+      console.error(err);
+      alert(err.message || 'Error creating user');
+    }
+  }
 
   return (
-    <SafeAreaView>
-      <TouchableOpacity onPress={closeForm}>
-        <Text style={{color:COLORS.BLUE}}>Go Back</Text>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity activeOpacity={1} onPress={Keyboard.dismiss} style={styles.container}>
+        <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+          <TouchableOpacity style={{margin:20}} onPress={() => setIsRenderForm(false)}>
+            <Text style={{color:COLORS.BLUE}}>Go Back</Text>
+          </TouchableOpacity>
+          <View style={userFormStyles.formContainer}>
+            <Text>Name <Text style={{color:'red'}}>*</Text></Text>
+            <TextInput value={name} style={userFormStyles.textInput} placeholder="Sgt. Slaughter *" onChangeText={text => setName(text)} />
+          </View>
+          <View style={userFormStyles.formContainer}>
+            <Text>Age</Text>
+            <TextInput value={age} keyboardType="number-pad" style={userFormStyles.textInput} placeholder="26" onChangeText={text => setAge(text)} />
+          </View>
+          <Text>{color}</Text>
+          <View style={userFormStyles.colorwheelContainer}>
+            <ColorWheel
+              initialColor={INITIAL_COLOR}
+              onColorChange={color => console.log({color})}
+              onColorChangeComplete={onColorChangeComplete}
+              // style={userFormStyles.colorwheelContainer}
+              thumbStyle={userFormStyles.colorwheelThumb}
+            />
+          </View>
+          <TouchableOpacity style={userFormStyles.submitBtn} onPress={doCreateUser}>
+            <Text style={{color:'white'}}>Create User</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
       </TouchableOpacity>
     </SafeAreaView>
   )
 }
+// #endregion USER_FORM
 
+// #region USER
 const userStyles = StyleSheet.create({
   addUserBtn: {
     position: 'absolute',
@@ -52,50 +145,95 @@ const userStyles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 20,
     marginLeft: 20,
+    marginBottom: 10,
     width: getWidth(),
   },
 });
 
-// #region USER
+function handleUserOnData({ client, subscriptionData:{ data } }, refetch) {  
+  /** Commented out to show logic could possibly do but we'll just refetch
+   * in which case we're really better off polling!!
+   */
+  // const res = client.readQuery({
+  //   query: listUsers
+  // });
+
+  // const [key] = Object.keys(data);
+  // switch (key) {
+  //   case 'onCreateUser':
+  //     console.log('onCreateUser', data);
+  //     res.listUsers.filter(user => user.id !== data.onCreateUser.id).concat(data.onCreateUser);
+  //     break;
+  //   case 'onUpdateUser':
+  //     console.log('onUpdateUser', data);
+  //     res.listUsers.filter(user => user.id !== data.onUpdateUser.id).concat(data.onUpdateUser);
+  //     break;
+  //   case 'onDeleteUser':
+  //     console.warn('onDeleteUser subscriptions returns null... Possible bug??');
+  //     break;
+  //   default:
+  //     break;
+  // }
+
+  // client.writeQuery({
+  //   query: listUsers,
+  //   data: res,
+  // });
+  
+  refetch();
+}
+
+function renderListEmpty() {
+  return (
+    <View style={userStyles.centerFlex}>
+      <Text>No Users</Text>
+    </View>
+  );
+}
+
+function renderUser({ item: user }) {
+  return <UserCard user={user} />;
+}
+
+function renderLoading() {
+  return (
+    <View style={userStyles.centerFlex}>
+      <Text>Loading...</Text>
+    </View>
+  );
+}
+
+function renderError() {
+  return (
+    <View style={userStyles.centerFlex}>
+      <Text style={{color:'red'}}>{JSON.stringify(error, null, 5)}</Text>
+    </View>
+  );
+}
+
 function User() {
-  const {data, loading, refetch, error} = useQuery(listUsers, {fetchPolicy:'cache-and-network'});
   const [isRenderForm, setIsRenderForm] = useState(false);
 
-  if (isRenderForm) {
-    return (
-      <UserForm setIsRenderForm={setIsRenderForm} />
-    )
-  }
+  const {data, loading, refetch, error} = useQuery(listUsers, {fetchPolicy:'cache-and-network'});
+  useSubscription(onCreateUser, { onSubscriptionData: data => handleUserOnData(data, refetch) });
+  useSubscription(onUpdateUser, { onSubscriptionData: data => handleUserOnData(data, refetch) });
+  useSubscription(onDeleteUser, { onSubscriptionData: data => handleUserOnData(data, refetch) });
 
-  console.log(data, loading);
+  if (isRenderForm) return <UserForm setIsRenderForm={setIsRenderForm} />;
 
-  if (loading) {
-    return (
-      <View style={userStyles.centerFlex}>
-        <Text>Loading...</Text>
-      </View>
-    );
-  }
+  if (loading) return renderLoading();
 
-  if (error) {
-    return (
-      <View style={userStyles.centerFlex}>
-        <Text style={{color:'red'}}>{JSON.stringify(error, null, 5)}</Text>
-      </View>
-    );
-  }
+  if (error) return renderError();
   
   const users = data ? data.listUsers : [];
 
   if (!users.length) return renderListEmpty();
 
-  const openForm = () => setIsRenderForm(true);
-
   return (
-    <SafeAreaView>
+    <SafeAreaView style={[styles.container]}>
       <View style={[userStyles.header]}>
         <Text style={{fontSize:28, fontWeight:'bold'}}>{`${users.length} Users`}</Text>
-        <TouchableOpacity onPress={openForm} style={[styles.center, userStyles.addUserBtn]}>
+        <TouchableOpacity onPress={() => setIsRenderForm(true)} style={[styles.center, userStyles.addUserBtn]}>
           <Text style={{color:'white'}}>Add User</Text>
         </TouchableOpacity>
       </View>
@@ -111,24 +249,11 @@ function User() {
     </SafeAreaView>
   )
 }
-
-function renderListEmpty() {
-  return (
-    <View style={userStyles.centerFlex}>
-      <Text>No Users</Text>
-    </View>
-  )
-}
-
-function renderUser({ item: user }) {
-  return (
-    <UserCard user={user} />
-  )
-}
 // #endregion USER
 
+// #region USER_CARD
 const CARD_MARGIN = 20;
-const CARD_HEIGHT_PERCENTAGE = .5;
+const CARD_HEIGHT_PERCENTAGE = .5; // how big card is out of screen height
 
 const cardStyles = StyleSheet.create({
   container: {
@@ -143,7 +268,6 @@ const cardStyles = StyleSheet.create({
     fontWeight:'bold',
   },
   btnContainer: {
-    // flex: 1,
     flexDirection: 'row',
   }
 });
@@ -155,6 +279,7 @@ function UserCard({
 }) {
   const width = (getWidth() / NUM_COLUMNS) - (CARD_MARGIN * 2);
   const height = getHeight();
+
   return (
     <View style={[cardStyles.container]}>
       {/* Header */}
@@ -168,14 +293,15 @@ function UserCard({
       {/* Action Btn Block */}
       <View style={[cardStyles.btnContainer]}>
         <TouchableOpacity style={[styles.center, { borderColor: COLORS.BLUE, borderRadius:4, borderWidth:1, width: (width / 2)-1, height: height * CARD_HEIGHT_PERCENTAGE * 0.15 }]}>
-          <Text>Edit</Text>
+          <Text style={{ color:COLORS.BLUE }}>Edit</Text>
         </TouchableOpacity>
         <TouchableOpacity style={[styles.center, { borderColor: COLORS.RED, borderRadius:4, borderWidth:1, width: (width / 2)-1, height: height * CARD_HEIGHT_PERCENTAGE * 0.15 }]}>
-          <Text>Delete</Text>
+          <Text style={{ color:COLORS.RED }}>Delete</Text>
         </TouchableOpacity>
       </View>
     </View>
   )
 }
+// #endregion USER_CARD
 
 export default User;
